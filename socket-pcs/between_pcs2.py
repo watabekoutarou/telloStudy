1c44911bee12bab3e0839690ccf2207420c917ab
watawo4 <107882109+watabekoutarou@users.noreply.github.com>
2023-06-03 17:06:18 +0900

    Update between_pcs2.py

--- socket-pcs/between_pcs2.py
+++ socket-pcs/between_pcs2.py
@@ -4,11 +4,11 @@ import time
 
 #https://qiita.com/note-tech/items/c3e1e497d231ea1e7ca7 引用
 #pc2側(server)のプログラム
-M_size = 1024
+M_size = 65535
 
 #pc1がわの情報
-host = '0.0.0.0'
-port = 8890
+host = ''
+port = 8080
 pc1Adrres = (host, port)
 
 sock = socket.socket(socket.AF_INET, type=socket.SOCK_DGRAM)
@@ -20,6 +20,7 @@ while True:
     try :
         # ③Clientからのmessageの受付開始
         print('Waiting message')
+        #~ここまでできている
         message, cli_addr = sock.recvfrom(M_size)
         message = message.decode(encoding='utf-8')
         print(f'Received message is [{message}]')
