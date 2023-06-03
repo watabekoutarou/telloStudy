19e2c120b0671101777ee0647828ce89b0237ed6
watawo4 <107882109+watabekoutarou@users.noreply.github.com>
2023-06-03 17:06:41 +0900

    Update between_pcs.py

--- socket-pcs/between_pcs.py
+++ socket-pcs/between_pcs.py
@@ -3,18 +3,22 @@
 #ソケット通信によりPC!とPC2を接続
 #こちら側がPC1,(cliant) 
 import socket
-M_SIZE = 1024
+M_SIZE = 65535
 #pc2側のアドレス
-pc2_address = ('192.168.11.6',8890)
+
+pc2_address = ('127.0.0.1',8080)
 #socket作成　ip6ならAF_INET6似
 socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 while True:
     try:
-        message = ("send message")
-        #
+        message = ("send message from mac")
+        #  
         if message != 'end':
-            socket.sendto(message.encode('utf-8'),pc2_address)
+            #送る側
+            #メッセージを送る部分
+            #send_len = socket.sendto(message.encode(),pc2_address)
+            socket.sendto(message.encode(),pc2_address)
             print("Wait response")
             rx_mes, addr = socket.recvfrom(M_SIZE)
             print(f"[Server]: {rx_mes.decode(encoding='utf-8')}")
