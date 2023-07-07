ac9f732ea078592202f80f624792dcffa234600e
watawo4 <107882109+watabekoutarou@users.noreply.github.com>
2023-07-07 12:58:44 +0900

    Create server_dorone.py

---
+++ tello_video_test1/server_dorone.py
@@ -0,0 +1,172 @@
+#server_drone.py
+import socket
+import time
+from PIL import Image,ImageTk
+import tkinter as tk
+import io
+import pickle
+import torch
+import torch.cuda
+import numpy as np
+import cv2
+import sys
+#物体が写る画像1と物体の検出範囲が同一で100mm前進した画像によって距離の推定を行う(ただし物体と同じクラスは検出されないものとする)
+
+M_size = 65535
+def square(xmin,xmax,ymin,ymax):
+  return (xmax-xmin)*(ymax-ymin)
+
+#pc1がわの情報
+host = '192.168.11.6'
+port = 8080
+serverAddress = (host, port)
+
+sock = socket.socket(socket.AF_INET, type=socket.SOCK_STREAM)
+print('create socket')
+sock.bind(serverAddress)
+sock.listen()
+
+
+#model の読み込み
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu" )
+model = torch.hub.load('ultralytics/yolov5','yolov5s')
+model.to(device)
+model.eval()
+print("接続待機")
+
+client_socket, client_address = sock.accept()
+
+print("接続:",client_address)
+A = 0
+B = 99999
+x = 200
+
+def analyDistance(A,B):
+    #print(f"A is {A},B is {B}")
+    distance = str(int(B/(B-A)*x))
+    print(distance,type(distance))
+    response = distance.encode('utf-8')
+    client_socket.send(response)
+
+while True:
+  try:
+    message = client_socket.recv(M_size)
+    #復元
+    data = pickle.loads(message)
+    #Bytesloから
+    content = data.getvalue()
+    #画像を保存
+    img = Image.open(io.BytesIO(content))
+    img = img.resize((640,480))
+    #img.save("test3.jpg")
+    result = model(img)
+    result.render()
+    result.show()
+    #obj に推論の結果の集合を代入
+    obj = result.pandas().xyxy[0]
+    #推論の結果のバウンディングボックスのクラスネームと座標を出力
+    dic = {}
+    for i in range(len(obj)):
+      name = obj.name[i]
+      xmin = obj.xmin[i]
+      ymin = obj.ymin[i]
+      xmax = obj.xmax[i]
+      ymax = obj.ymax[i]
+      # 元の物体の高さ（ピくセル）を　A、後をBに
+      if name =="bottle" :
+        A = ymax-ymin
+        #対象があった場合clientに特別なメッセージを送信
+        response = "discover"
+        client_socket.send(response.encode('utf-8'))
+        #対象があった場合distanceの計算のため
+        # 2回目の画像送信を受信する
+        message = client_socket.recv(M_size)
+        data = pickle.loads(message)
+        content = data.getvalue()
+        #画像を保存
+        img = Image.open(io.BytesIO(content))
+        img = img.resize((640,480))
+        result = model(img)
+        result.render()
+        #result.show()
+        #obj に推論の結果の集合を代入
+        obj = result.pandas().xyxy[0]
+        for i in range(len(obj)):
+            name = obj.name[i]
+            xmin = obj.xmin[i]
+            ymin = obj.ymin[i]
+            xmax = obj.xmax[i]
+            ymax = obj.ymax[i]
+            if name =="bottle" :
+                B = ymax-ymin
+                break
+        if B != 99999:
+           distance = analyDistance(A,B)
+           response = distance.encode('utf-8')
+           client_socket.send(response)
+        
+        B=99999
+        message = client_socket.recv(M_size)
+        #復元
+        data = pickle.loads(message)
+        #Bytesloから
+        content = data.getvalue()
+        #画像を保存
+        img = Image.open(io.BytesIO(content))
+        img = img.resize((640,480))
+        #img.save("test3.jpg")
+        result = model(img)
+        result.render()
+        result.show()
+        #obj に推論の結果の集合を代入
+        obj = result.pandas().xyxy[0]
+        #推論の結果のバウンディングボックスのクラスネームと座標を出力
+        for i in range(len(obj)):
+            name = obj.name[i]
+            xmin = obj.xmin[i]
+            ymin = obj.ymin[i]
+            xmax = obj.xmax[i]
+            ymax = obj.ymax[i]
+        # 元の物体の高さ（ピくセル）を　A、後をBに
+            if name =="bottle" :
+              A = ymax-ymin
+              message = client_socket.recv(M_size)
+              data = pickle.loads(message)
+              content = data.getvalue()
+              #画像を保存
+              img = Image.open(io.BytesIO(content))
+              img = img.resize((640,480))
+              result = model(img)
+              result.render()
+              #result.show()
+              #obj に推論の結果の集合を代入
+              obj = result.pandas().xyxy[0]
+              for i in range(len(obj)):
+                  name = obj.name[i]
+                  xmin = obj.xmin[i]
+                  ymin = obj.ymin[i]
+                  xmax = obj.xmax[i]
+                  ymax = obj.ymax[i]
+                  if name =="bottle" :
+                      B = ymax-ymin
+                      break
+                  if B ==99999:
+                     response = "end"
+                     client_socket.send(response.encode('utf-8'))
+                     sys.exit()
+                  analyDistance(A,B)
+        else:
+           response = "end"
+           response = response.encode('utf-8')
+           client_socket.send(response)
+           sys.exit()
+      else:
+         response="None"
+         client_address.send(response.encode('utf-8'))
+         continue
+         
+  except KeyboardInterrupt:
+    print ('\n . . .\n')
+    client_socket.close()
+    sock.close()
+    break
